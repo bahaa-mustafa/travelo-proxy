1fc8fec834955eb0efc9a7bf0abd96c9a840f116
bahaa-mustafa <bahaamustafabadr@gmail.com>
2026-02-04 14:24:53 +0200

    add endpoint for chat bot

--- server.js
+++ server.js
@@ -1,71 +1,9 @@
-// import express from "express";
-// import fetch from "node-fetch";
-// import cors from "cors";
-
-// const app = express();
-// const BACKEND_BASE_URL = "http://traveloo.runasp.net";
-
-// const corsOptions = {
-//   origin: "https://travelo-t.netlify.app",
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
-
-// app.use(express.json());
-
-// app.use("/api", async (req, res) => {
-//   try {
-//     const targetUrl = BACKEND_BASE_URL + req.originalUrl.replace("/api", "");
-
-//     const response = await fetch(targetUrl, {
-//       method: req.method,
-//       headers: {
-//         "Content-Type": "application/json",
-//         ...(req.headers.authorization && {
-//           Authorization: req.headers.authorization,
-//         }),
-//       },
-//       body:
-//         req.method === "GET" || req.method === "HEAD"
-//           ? undefined
-//           : JSON.stringify(req.body),
-//     });
-
-//     const text = await response.text();
-
-
-//     res.setHeader(
-//       "Access-Control-Allow-Origin",
-//       "https://travelo-t.netlify.app"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Content-Type, Authorization"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, PUT, DELETE, OPTIONS"
-//     );
-//     res.setHeader("Vary", "Origin");
-
-//     res.status(response.status).send(text);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Proxy server error" });
-//   }
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log("✅ Proxy running on port", PORT);
-// });
-
 
 import express from "express";
 import fetch from "node-fetch";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const BACKEND_BASE_URL = "http://traveloo.runasp.net";
@@ -99,6 +37,29 @@ app.use((req, res, next) => {
 /**
  * 🔁 GENERIC PROXY
  */
+
+// Ai chat bot
+app.post("/ai/chatbot", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://ayamotawea-Tourism-Chatbot.hf.space/answer",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.HF_TOKEN}`
+        },
+        body: JSON.stringify({ text: req.body.text })
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Chatbot failed" });
+  }
+});
+
 
 // images
 app.use("/img", async (req, res) => {
